07dcc2b270749bc647ef37c64b5e97bc1a60b82a
Wei Lu <wei@yboom.com>
2024-01-22 10:59:04 +0200

    prepare for more actions

--- crawler.py
+++ crawler.py
@@ -6,27 +6,32 @@ from datafile import DataFileHandler
 import random
 import time
 
-class Crawler:
-    def __init__(self, driver, site_model, query):
-        self.driver = driver
-        self.site_model = site_model
-        self.query = query
+class Action:
+    def __init__(self):
+        pass
 
-        # 提取网站名称作为site参数
-        site = self.extract_site_name(site_model['url'])
-        self.data_handler = DataFileHandler(main_key=site_model['main_key'], site=site, query=self.query)
+    def execute(self, driver):
+        raise NotImplementedError("Execute method should be implemented by subclasses")
+
+class RecordItems(Action):
+    def __init__(self, url, query, items_sel, item_selectors, main_key, next_pg_sel, open_details, details_selectors, close_details):
+        super().__init__()
+        self.items_sel = items_sel
+        self.item_selectors = item_selectors
+        self.main_key = main_key
+        self.next_pg_sel = next_pg_sel
+        self.open_details = open_details
+        self.details_selectors = details_selectors
+        self.close_details = close_details
+        self.url = url
+        self.query = query
+        site = self.extract_site_name(url)
+        self.data_handler = DataFileHandler(main_key=main_key, site=site, query=query)
 
     def extract_site_name(self, url):
-        # 实现从URL中提取网站名称的逻辑
-        # 例如：从 "https://www.upwork.com/nx/search/jobs/..." 中提取 "upwork"
-        # 这里的实现可能需要根据实际URL格式进行调整
         return url.split("//")[1].split("/")[0].split(".")[-2]
 
-    def run(self):
-        url = self.site_model['url']
-        self.driver.get(url)
-        time.sleep(2)
-
+    def execute(self, driver):
         counter = 0
         load_next_page = True
         last_pg_txt = ''
@@ -35,8 +40,8 @@ class Crawler:
             try:
                 found = False
                 for _ in range(6):
-                    WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.site_model['items_sel'])))
-                    items = self.driver.find_elements(By.CSS_SELECTOR, self.site_model['items_sel'])
+                    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.items_sel)))
+                    items = driver.find_elements(By.CSS_SELECTOR, self.items_sel)
                     if items and items[-1].text != last_pg_txt:
                         last_pg_txt = items[-1].text
                         found = True
@@ -53,58 +58,74 @@ class Crawler:
                 break
 
             for i, el in enumerate(items):
-                data = self.extract_item_data(el)
+                data = self.extract_item_data(el, driver)
 
-                # 检查是否有重复的数据
-                if self.data_handler.is_duplicate(data.get(self.site_model['main_key'], '')):
-                    print("Over w/ is_duplicate")
+                # Check for duplicates
+                if self.data_handler.is_duplicate(data.get(self.main_key, '')):
+                    print("Duplicate found, stopping.")
                     load_next_page = False
                     break
 
-                if not data.get(self.site_model['main_key'], ''):
-                    break;
+                if not data.get(self.main_key, ''):
+                    break
 
                 print(data)        
-
                 self.data_handler.append_data(data)
 
             if load_next_page:
-                try:
-                    self.go_next_page()
-                except Exception as e:
-                    break
+                load_next_page = self.go_next_page(driver)
+
                 time.sleep(random.randrange(1, 5))
-
                 counter += 1
-                print('page:', counter)
+                print('Page:', counter)
 
-        self.data_handler.save_main_file()
+    def go_next_page(self, driver):
+        try:
+            nextButton = driver.find_elements(By.CSS_SELECTOR, self.next_pg_sel)
+            if not nextButton:
+                return False
+            driver.click_element(nextButton[0])
+            return True
+        except Exception as e:
+            print("Error moving to next page:", e)
+            return False
 
-    def go_next_page(self):
-        nextButton = self.driver.find_elements(By.CSS_SELECTOR, self.site_model['next_pg_sel'])
-        if not nextButton:
-            raise Exception('Not found', self.site_model['next_pg_sel'])
-        self.driver.click_element(nextButton[0])
-
-    def extract_item_data(self, el):
+    def extract_item_data(self, el, driver):
         data = {}
 
-        for key, sel in self.site_model['item_selectors'].items():
-            data[key] = self.driver.extract_data(el, sel)
+        for key, sel in self.item_selectors.items():
+            data[key] = driver.extract_data(el, sel)#from el
 
-        if self.site_model['detail_open_sel']:
+        # Handle details if necessary
+        if self.open_details:
             try:
-                elem = el.find_element(By.CSS_SELECTOR, self.site_model['detail_open_sel'])
-                self.driver.click_element(elem)
+                if 'selector' in self.open_details:
+                    elem = el.find_element(By.CSS_SELECTOR, self.open_details['selector'])
+                    self.driver.click_element(elem)
                 time.sleep(2)
-                for k, s in self.site_model['details_selectors'].items():
-                    data[k] = self.driver.extract_data(self.driver, s)
+                for k, s in self.details_selectors.items():
+                    data[k] = driver.extract_data(driver, s)#from entire document
 
-                if self.site_model['detail_close_sel']:
-                    close = self.driver.find_element(By.CSS_SELECTOR, self.site_model['detail_close_sel'])
-                    self.driver.click_element(close)
+                if self.close_details:
+                    if 'selector' in self.close_details:
+                        close = self.driver.find_element(By.CSS_SELECTOR, self.close_details['selector'])
+                        self.driver.click_element(close)
             except Exception as e:
-                for k, s in self.site_model['details_selectors'].items():
+                for k, s in self.details_selectors.items():
                     data[k] = ""
+
         return data
 
+class Crawler:
+    def __init__(self, driver, url, query, actions):
+        self.driver = driver
+        self.url = url
+        self.actions = actions
+        self.query = query
+
+    def run(self):
+        self.driver.get(self.url)
+        time.sleep(2)
+
+        for action in self.actions:
+            action.execute(self.driver)
--- crawler_upwork.py
+++ crawler_upwork.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.chrome.options import Options
 from compatibledriver import ChromeDriver, SafariDriver
-from crawler import Crawler
+from crawler import Crawler, RecordItems
 
 import argparse
 
@@ -29,40 +29,44 @@ if len(args.keyword)<1:
 else:
     keyword = args.keyword
 
+url = f'https://www.upwork.com/nx/search/jobs/?nbs=1&page=1&q={keyword}&sort=recency'
 #TODO: LLM to find these
-site_model = {
-    'url' : f'https://www.upwork.com/nx/search/jobs/?nbs=1&page=1&q={keyword}&sort=recency',
-    'items_sel' : "section[data-test='JobsList'] > article",
-    'item_selectors' : {
-        "posted_time": "div.job-tile-header small span:nth-of-type(2)",
-        "job_title": "h2.job-tile-title a",
-        "job_type": "li[data-test='job-type-label']",
-        "experience_level": "li[data-test='experience-level']",
-        "duration_hours": "li[data-test='duration-label']",
-        "job_description": "div[data-test='JobDescription'] p",
-        "skill_tags": {
-            "selector": "span[data-test='token'] span",
-            "type": "multi-text"
-        },
-        "job_link": {
-            "selector": "h2.job-tile-title a[href]",
-            "type": "attr"
-        }
-    },
-    'main_key' : "job_link",
-    'next_pg_sel' : "button[data-ev-label='pagination_next_page']",
-    'detail_open_sel' : "h2.job-tile-title a",
-    'details_selectors' : {
-        "location":'div.job-details-loader div[data-test="LocationLabel"]',
-        "client_location": "div.job-details-loader li[data-qa='client-location']",
-        "client_job_posting_stats": "div.job-details-loader li[data-qa='client-job-posting-stats']",
-        "client_activity": 'div.job-details-loader ul.client-activity-items',
-        "client_contract_date": 'div.job-details-loader div[data-qa="client-contract-date"]'
-    },
-    'detail_close_sel' : "button[data-test='slider-close-desktop']",
-}
+actions=[
+        RecordItems(
+            url = url,
+            query = keyword,
+            items_sel = "section[data-test='JobsList'] > article",
+            item_selectors = {
+                "posted_time": "div.job-tile-header small span:nth-of-type(2)",
+                "job_title": "h2.job-tile-title a",
+                "job_type": "li[data-test='job-type-label']",
+                "experience_level": "li[data-test='experience-level']",
+                "duration_hours": "li[data-test='duration-label']",
+                "job_description": "div[data-test*='JobDescription'] p",
+                "skill_tags": {
+                    "selector": "span[data-test='token'] span",
+                    "type": "multi-text"
+                },
+                "job_link": {
+                    "selector": "h2.job-tile-title a[href]",
+                    "type": "attr"
+                }
+            },
+            main_key = "job_link",
+            next_pg_sel = "button[data-ev-label='pagination_next_page']",
+            open_details = {'selector':"h2.job-tile-title a"},
+            details_selectors = {
+                "location":'div.job-details-loader div[data-test="LocationLabel"]',
+                "client_location": "div.job-details-loader li[data-qa='client-location']",
+                "client_job_posting_stats": "div.job-details-loader li[data-qa='client-job-posting-stats']",
+                "client_activity": 'div.job-details-loader ul.client-activity-items',
+                "client_contract_date": 'div.job-details-loader div[data-qa="client-contract-date"]'
+            },
+            close_details = {'selector':"button[data-test='slider-close-desktop']"},        
+        )
+    ]
 
-crawler = Crawler(driver, site_model, query=keyword)
+crawler = Crawler(driver, url = url, query=keyword, actions=actions)
 
 crawler.run()
 driver.quit()
